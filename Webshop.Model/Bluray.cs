310a38c259508b6f425b58e1b806a9061ed13346
Davy Gevaert <davy.gevaert@student.vives.be>
2023-11-25 16:03:58 +0100

    Add project files.

---
+++ Webshop.Model/Bluray.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Webshop.Model
+{
+    public class Bluray
+    {
+        public int Id { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? Description { get; set; }
+
+        public string? TrailerURL { get; set; }
+
+        public int Price { get; set; }
+    }
+}
---
+++ Webshop.Repository/WebshopDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Webshop.Model;
+
+namespace Webshop.Repository
+{
+    public class WebshopDbContext : DbContext
+    {
+        public static string LocalConnectionString { get; set; } =
+            @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=WebshopDB;Integrated Security=True";
+
+        public WebshopDbContext(DbContextOptions<WebshopDbContext> options) : base(options)
+        {
+
+        }
+
+        public WebshopDbContext()
+        {
+
+        }
+
+        public DbSet<Bluray> Blurays { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(LocalConnectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
