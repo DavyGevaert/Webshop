585dc0a7ac346d7889616b2faccc6ddf682e2c1d
DavyGevaert <gevaert.davy@gmail.com>
2024-04-09 15:51:41 +0200

    commit push additional files

---
+++ Webshop.Api/Controllers/IdentityController.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Webshop.Api.Authentication.Abstractions;
+using Webshop.Api.Authentication.Model;
+
+namespace Webshop.Api.Controllers
+{
+	[ApiController]
+	[Authorize]
+	public class IdentityController : ControllerBase
+	{
+		private readonly IIdentityService _identityService;
+
+		public IdentityController(IIdentityService identityService)
+		{
+			_identityService = identityService;
+		}
+
+		[HttpPost("identity/sign-in")]
+		public async Task<IActionResult> SignIn(UserSignInRequest request)
+		{
+			var authenticationResult = await _identityService.SignIn(request);
+			return Ok(authenticationResult);
+		}
+
+		[HttpPost("identity/register")]
+		public async Task<IActionResult> Register(UserRegistrationRequest request)
+		{
+			var authenticationResult = await _identityService.Register(request);
+			return Ok(authenticationResult);
+		}
+	}
+}
---
+++ Webshop.UI.BlazorApp/Stores/TokenStore.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+
+namespace PeopleManager.Ui.Mvc.Stores
+{
+	public class TokenStore
+	{
+		private readonly IHttpContextAccessor _contextAccessor;
+
+		public TokenStore(IHttpContextAccessor contextAccessor)
+		{
+			_contextAccessor = contextAccessor;
+		}
+
+		public string GetToken()
+		{
+			if (_contextAccessor.HttpContext is null)
+			{
+				return null;
+			}
+
+			if (!_contextAccessor.HttpContext.Request.Cookies.ContainsKey("Token"))
+			{
+				return null;
+			}
+
+			if (!_contextAccessor.HttpContext.Request.Cookies.TryGetValue("Token", out string token))
+			{
+				return null;
+			}
+
+			return token;
+		}
+
+		public void SaveToken(string token)
+		{
+			if (_contextAccessor.HttpContext is null)
+			{
+				return;
+			}
+
+			Clear();
+
+			_contextAccessor.HttpContext.Response.Cookies.Append("Token", token,
+				new CookieOptions
+				{
+					HttpOnly = true,
+					Expires = DateTime.UtcNow.AddHours(8),
+					Secure = true
+				});
+		}
+
+		public void Clear()
+		{
+			if (_contextAccessor.HttpContext is null)
+			{
+				return;
+			}
+
+			if (_contextAccessor.HttpContext.Request.Cookies.ContainsKey("Token"))
+			{
+				_contextAccessor.HttpContext.Response.Cookies.Delete("Token");
+			}
+		}
+	}
+}
